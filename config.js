0e0399ee09b0be200d5cfe4c52a93db09fabd3da
Dryve Admin Support <admin.support@dryve.ph>
2026-01-30 17:56:16 +0800

    Add drive-list URL to config

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const config = {
-    "live-auction" : "https://e6b43ec7-3673-46a1-948b-9538f969a721-00-1x3iw41kiu5tj.worf.replit.dev/"
+    "live-auction" : "https://e6b43ec7-3673-46a1-948b-9538f969a721-00-1x3iw41kiu5tj.worf.replit.dev/",
+    "drve-list" : "https://drive.google.com/file/d/1slwBFLeTwhcuu1PM0-BVPGhl8RiQfa98/view?usp=sharing",
+
 }
