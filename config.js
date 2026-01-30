d3d3f615443fa79b56894e4bf8eb171ba7e65e75
Dryve Admin Support <admin.support@dryve.ph>
2026-01-30 18:01:46 +0800

    Refactor config.js for improved formatting
    Reformatted the config object for better readability.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    "live-auction" : "https://e6b43ec7-3673-46a1-948b-9538f969a721-00-1x3iw41kiu5tj.worf.replit.dev/",
-    "dryve-list" : "https://drive.google.com/file/d/1slwBFLeTwhcuu1PM0-BVPGhl8RiQfa98/view?usp=sharing",
-
-}
-
+  "live-auction":
+    "https://e6b43ec7-3673-46a1-948b-9538f969a721-00-1x3iw41kiu5tj.worf.replit.dev/",
+  "dryve-list":
+    "https://drive.google.com/file/d/1slwBFLeTwhcuu1PM0-BVPGhl8RiQfa98/view?usp=sharing",
+};
